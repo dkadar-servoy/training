0a7836fc1301bb6360b32cbd9b1e198bd31667b9
Daniel Kadar <dkadar@servoy.com>
2019-11-12 13:27:40 +0100

    Exercise 4 progress/cleanup

---
+++ work_space_exercise4/exercise4/forms/customersTable.js
@@ -0,0 +1,16 @@
+/**
+ * Called when the mouse is double clicked on a row/cell (foundset and column indexes are given).
+ *
+ * @param {Number} foundsetindex
+ * @param {Number} [columnindex]
+ * @param {JSRecord} [record]
+ * @param {JSEvent} [event]
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"E195EA0B-D6BA-45C7-858F-D33C32A97FA0"}
+ */
+function onCellDoubleClickGoToOrders(foundsetindex, columnindex, record, event) {
+	forms.ordersTable.filterFoundset(foundset.customerid);
+	application.showForm(forms.ordersTable);
+}
